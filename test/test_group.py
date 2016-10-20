02559aba26e8c5868e0d3c394bbeeb17b2c83fa1
Russell Hay <RussTheAerialist@users.noreply.github.com>
2016-10-20 14:14:29 -0700

    Making the user list operations a no-op if we haven't populated the list of users yet (#68)

--- test/test_group.py
+++ test/test_group.py
@@ -98,9 +98,17 @@ class GroupTests(unittest.TestCase):
         self.assertEqual('ServerAdministrator', user.site_role)
 
     def test_add_user_before_populating(self):
-        single_group = TSC.GroupItem('test')
-        self.assertRaises(TSC.UnpopulatedPropertyError, self.server.groups.add_user, single_group,
-                          '5de011f8-5aa9-4d5b-b991-f462c8dd6bb7')
+        with open(GET_XML, 'rb') as f:
+            get_xml_response = f.read().decode('utf-8')
+        with open(ADD_USER, 'rb') as f:
+            add_user_response = f.read().decode('utf-8')
+        with requests_mock.mock() as m:
+            m.get(self.baseurl, text=get_xml_response)
+            m.post('http://test/api/2.3/sites/dad65087-b08b-4603-af4e-2887b8aafc67/groups/ef8b19c0-43b6-11e6-af50'
+                   '-63f5805dbe3c/users', text=add_user_response)
+            all_groups, pagination_item = self.server.groups.get()
+            single_group = all_groups[0]
+            self.server.groups.add_user(single_group, '5de011f8-5aa9-4d5b-b991-f462c8dd6bb7')
 
     def test_add_user_missing_user_id(self):
         with open(POPULATE_USERS, 'rb') as f:
@@ -120,9 +128,16 @@ class GroupTests(unittest.TestCase):
                           '5de011f8-5aa9-4d5b-b991-f462c8dd6bb7')
 
     def test_remove_user_before_populating(self):
-        single_group = TSC.GroupItem('test')
-        self.assertRaises(TSC.UnpopulatedPropertyError, self.server.groups.remove_user, single_group,
-                          '5de011f8-5aa9-4d5b-b991-f462c8dd6bb7')
+        with open(GET_XML, 'rb') as f:
+            response_xml = f.read().decode('utf-8')
+        with requests_mock.mock() as m:
+            m.get(self.baseurl, text=response_xml)
+            m.delete('http://test/api/2.3/sites/dad65087-b08b-4603-af4e-2887b8aafc67/groups/ef8b19c0-43b6-11e6-af50'
+                     '-63f5805dbe3c/users/5de011f8-5aa9-4d5b-b991-f462c8dd6bb7',
+                     text='ok')
+            all_groups, pagination_item = self.server.groups.get()
+            single_group = all_groups[0]
+            self.server.groups.remove_user(single_group, '5de011f8-5aa9-4d5b-b991-f462c8dd6bb7')
 
     def test_remove_user_missing_user_id(self):
         with open(POPULATE_USERS, 'rb') as f:
