be85635a67d3faa499864289858358fd545d9ad7
geordielad <rcottiss@cottiss.com>
2016-10-27 16:30:54 -0400

    reverted tableau_auth.py

--- tableauserverclient/models/tableau_auth.py
+++ tableauserverclient/models/tableau_auth.py
@@ -1,12 +1,6 @@
 class TableauAuth(object):
-	"""Authentication attributes for signin request
-
-	Consider removing this object and other variables holding secrets 
-	as soon as possible after use to avoid them hanging around in memory.
-
-	"""
-	def __init__(self, username, password, site='', user_id_to_impersonate=None):
-        self.username = username
+    def __init__(self, username, password, site='', user_id_to_impersonate=None):
+        self.user_id_to_impersonate = user_id_to_impersonate
         self.password = password
         self.site = site
-        self.user_id_to_impersonate = user_id_to_impersonate
+        self.username = username
