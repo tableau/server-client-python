8d13cf43155e842b8fbbccba7abcb51aa736473e
geordielad <rcottiss@cottiss.com>
2016-10-27 16:34:53 -0400

    attempting to fix codestyle errors

--- tableauserverclient/models/connection_credentials.py
+++ tableauserverclient/models/connection_credentials.py
@@ -1,7 +1,7 @@
 class ConnectionCredentials(object):
 	"""Connection Credentials for Workbooks and Datasources publish request
 
-	Consider removing this object and other variables holding secrets 
+	Consider removing this object and other variables holding secrets
 	as soon as poobible after use to avoid them hanging around in memory.
 
 	"""
@@ -9,4 +9,3 @@ class ConnectionCredentials(object):
        	self.name = name
         self.password = password
         self.embed = embed
- 
