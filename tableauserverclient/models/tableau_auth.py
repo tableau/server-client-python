011a73aabdddc3ef07f6ca69672c81026515a595
geordielad <rcottiss@cottiss.com>
2016-10-27 16:11:39 -0400

    Fixed indenting

--- tableauserverclient/models/tableau_auth.py
+++ tableauserverclient/models/tableau_auth.py
@@ -5,7 +5,7 @@ class TableauAuth(object):
 	as soon as possible after use to avoid them hanging around in memory.
 
 	"""
-	    def __init__(self, username, password, site='', user_id_to_impersonate=None):
+	def __init__(self, username, password, site='', user_id_to_impersonate=None):
         self.username = username
         self.password = password
         self.site = site
