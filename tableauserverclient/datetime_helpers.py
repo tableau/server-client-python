ff23f08458acaf25602814a978200af5ca9fda34
Russell Hay <me@russellhay.com>
2016-11-17 10:33:35 -0800

    removing attempt to import pytz

--- tableauserverclient/datetime_helpers.py
+++ tableauserverclient/datetime_helpers.py
@@ -1,28 +1,25 @@
 import datetime
 
-try:
-    from pytz import utc
-except ImportError:
-    # If pytz is not installed, let's polyfill a UTC timezone so it all just works
-    # This code below is from the python documentation for tzinfo: https://docs.python.org/2.3/lib/datetime-tzinfo.html
-    ZERO = datetime.timedelta(0)
-    HOUR = datetime.timedelta(hours=1)
 
-    # A UTC class.
+# This code below is from the python documentation for tzinfo: https://docs.python.org/2.3/lib/datetime-tzinfo.html
+ZERO = datetime.timedelta(0)
+HOUR = datetime.timedelta(hours=1)
 
-    class UTC(datetime.tzinfo):
-        """UTC"""
+# A UTC class.
 
-        def utcoffset(self, dt):
-            return ZERO
+class UTC(datetime.tzinfo):
+    """UTC"""
 
-        def tzname(self, dt):
-            return "UTC"
+    def utcoffset(self, dt):
+        return ZERO
 
-        def dst(self, dt):
-            return ZERO
+    def tzname(self, dt):
+        return "UTC"
 
-    utc = UTC()
+    def dst(self, dt):
+        return ZERO
+
+utc = UTC()
 
 TABLEAU_DATE_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
 
