edd692d34abac281a8cbccab596d5e129dc577af
Lalith Vipulananthan <lalithv@gmail.com>
2017-01-23 18:34:50 +0900

    First attempt at tests for tableau_auth

---
+++ test/test_tableauauth_model.py
@@ -0,0 +1,25 @@
+import unittest
+import warnings
+import tableauserverclient as TSC
+
+
+class TableauAuthModelTests(unittest.TestCase):
+    def test_username_password_required(self):
+        with self.assertRaises(TypeError) as context_manager:
+            TSC.TableauAuth()
+
+        self.assertRegex(str(context_manager.exception),
+                         r'\'username\' and \'password\'')
+
+    def test_site_arg_raises_warning(self):
+        with warnings.catch_warnings(record=True) as w:
+            # Cause all warnings to always be triggered.
+            warnings.simplefilter("always")
+
+            tableau_auth = TSC.TableauAuth('user',
+                                           'password',
+                                           site='dad65087-b08b-4603-af4e-2887b8aafc67')
+
+            assert len(w) == 1
+            assert issubclass(w[-1].category, DeprecationWarning)
+            assert "deprecated" in str(w[-1].message)
