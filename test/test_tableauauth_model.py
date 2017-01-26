8d9baa43afce3e5e1df18a2ae5a897bc78158f30
Lalith Vipulananthan <lalithv@gmail.com>
2017-01-26 15:57:29 +0900

    Updated following comments from t8y8

--- test/test_tableauauth_model.py
+++ test/test_tableauauth_model.py
@@ -4,19 +4,22 @@ import tableauserverclient as TSC
 
 
 class TableauAuthModelTests(unittest.TestCase):
+    def setUp(self):
+        self.auth = TSC.TableauAuth('user',
+                                    'password',
+                                    site_id='dad65087-b08b-4603-af4e-2887b8aafc67',
+                                    user_id_to_impersonate='admin')
+
     def test_username_password_required(self):
-        with self.assertRaises(TypeError) as context_manager:
+        with self.assertRaises(TypeError):
             TSC.TableauAuth()
 
     def test_site_arg_raises_warning(self):
         with warnings.catch_warnings(record=True) as w:
-            # Cause all warnings to always be triggered.
             warnings.simplefilter("always")
 
             tableau_auth = TSC.TableauAuth('user',
                                            'password',
-                                           site='dad65087-b08b-4603-af4e-2887b8aafc67')
+                                           site='Default')
 
-            assert len(w) == 1
-            assert issubclass(w[-1].category, DeprecationWarning)
-            assert "deprecated" in str(w[-1].message)
+            self.assertTrue(any(item.category == DeprecationWarning for item in w))
