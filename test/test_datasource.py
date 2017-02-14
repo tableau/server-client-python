884a6ab962db886392568f1ff82c1cd055ee5933
Tyler Doyle <t8y8@users.noreply.github.com>
2017-02-14 15:49:39 -0800

    Reword no_extract and update signature of added_in (#144)
    `parameter_added_in` got a facelift and now takes keyword arguments, where the value represents the version for that keyword. It looks a little cleaner and makes the checking code much simpler.
    
    Also renamed the `extract_only` to be `no_extract` because it means the opposite of `extract_only`, oops.

--- test/test_datasource.py
+++ test/test_datasource.py
@@ -173,7 +173,7 @@ class DatasourceTests(unittest.TestCase):
             m.get(self.baseurl + '/9dbd2263-16b5-46e1-9c43-a76bb8ab65fb/content?includeExtract=False',
                   headers={'Content-Disposition': 'name="tableau_datasource"; filename="Sample datasource.tds"'},
                   complete_qs=True)
-            file_path = self.server.datasources.download('9dbd2263-16b5-46e1-9c43-a76bb8ab65fb', extract_only=True)
+            file_path = self.server.datasources.download('9dbd2263-16b5-46e1-9c43-a76bb8ab65fb', no_extract=True)
             self.assertTrue(os.path.exists(file_path))
         os.remove(file_path)
 
