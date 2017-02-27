2796c37049ecd6ceed634a86d041f7d4174c0736
Chris Toomey <cmtoomey@users.noreply.github.com>
2017-02-27 14:51:53 -0800

    cleaned up c/p error and added str() to port

--- tableauserverclient/server/request_factory.py
+++ tableauserverclient/server/request_factory.py
@@ -321,7 +321,7 @@ class WorkbookRequest(object):
         if connection_item.server_address:
             connection_element.attrib['serverAddress'] = connection_item.server_address.lower()
         if connection_item.server_port:
-            connection_element.attrib['port'] = connection_item.server_address
+            connection_element.attrib['port'] = str(connection_item.server_port)
         if connection_item.username:
             connection_element.attrib['userName'] = connection_item.username
         if connection_item.password:
