0317e3e7d36d390ada3895ee13fe3df63ac2f73f
Russell Hay <RussTheAerialist@users.noreply.github.com>
2017-03-24 14:13:33 -0700

    Extract refresh support (#159)
    * Response to code reviews. Put all request options into 1 file. renamed sample to download_view_image.py. Comments clean up
    
    * Add api annotation to all current endpoints (#125)
    
    Part two of #125 
    
    This backfills all existing APIs with their minimum version.
    Checking this in early in release cycle for baketime.
    
    * initial implement of get all and get specific for Extract Refresh Tasks
    
    * fixing test failure in the schedule_item code
    
    * pep8 fixes
    
    * Download with extract_only and parameter checking (#143)
    
    * Add a new decorator that checks parameters against the version. Used with the @api decorator.
    * Add extract_only flags to workbooks and data sources, protected behind v2.5 flag
    
    * Correct the path to extract refresh tasks
    
    * fixing missed pep8 failure
    
    * adding runNow to the interface
    
    * fixing pep8 issues
    
    * Add header documentation to the sample.
    
    * addressing tyler's feedback

--- tableauserverclient/__init__.py
+++ tableauserverclient/__init__.py
@@ -2,7 +2,7 @@ from .namespace import NAMESPACE
 from .models import ConnectionCredentials, ConnectionItem, DatasourceItem,\
     GroupItem, PaginationItem, ProjectItem, ScheduleItem, \
     SiteItem, TableauAuth, UserItem, ViewItem, WorkbookItem, UnpopulatedPropertyError, \
-    HourlyInterval, DailyInterval, WeeklyInterval, MonthlyInterval, IntervalItem
+    HourlyInterval, DailyInterval, WeeklyInterval, MonthlyInterval, IntervalItem, TaskItem
 from .server import RequestOptions, ImageRequestOptions, Filter, Sort, Server, ServerResponseError,\
     MissingRequiredFieldError, NotSignedInError, Pager
 
--- tableauserverclient/server/request_options.py
+++ tableauserverclient/server/request_options.py
@@ -75,3 +75,23 @@ class ImageRequestOptions(RequestOptionsBase):
             params.append('resolution={0}'.format(self.imageresolution))
 
         return "{0}?{1}".format(url, '&'.join(params))
+
+
+class ImageRequestOptions(RequestOptionsBase):
+    # if 'high' isn't specified, the REST API endpoint returns an image with standard resolution
+    class Resolution:
+        High = 'high'
+
+    def __init__(self, imageresolution=None):
+        self.imageresolution = imageresolution
+
+    def image_resolution(self, imageresolution):
+        self.imageresolution = imageresolution
+        return self
+
+    def apply_query_params(self, url):
+        params = []
+        if self.image_resolution:
+            params.append('resolution={0}'.format(self.imageresolution))
+
+        return "{0}?{1}".format(url, '&'.join(params))
