9e94f1f52f30f11526477fc07e53f5549dec7afa
Russell Hay <me@russellhay.com>
2017-04-11 13:24:34 -0700

    fix mistaken version configuration

--- tableauserverclient/_version.py
+++ tableauserverclient/_version.py
@@ -41,7 +41,7 @@ def get_config():
     cfg = VersioneerConfig()
     cfg.VCS = "git"
     cfg.style = "pep440"
-    cfg.tag_prefix = "v®"
+    cfg.tag_prefix = "v"
     cfg.parentdir_prefix = "None"
     cfg.versionfile_source = "tableauserverclient/_version.py"
     cfg.verbose = False
