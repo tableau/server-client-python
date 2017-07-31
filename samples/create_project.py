ed6566b9ab81fb71ea787e534efda9fbdb700476
Jim Morris <jimorris@tableau.com>
2017-07-31 11:25:33 -0700

    Add parent Id to create request and add nested project sample

---
+++ samples/create_project.py
@@ -0,0 +1,65 @@
+####
+# This script demonstrates how to use the Tableau Server Client
+# to create new projects.
+#
+#
+# To run the script, you must have installed Python 2.7.X or 3.3 and later.
+####
+
+import argparse
+import getpass
+import logging
+import sys
+
+import tableauserverclient as TSC
+
+
+def create_project(server, project_item):
+    try:
+        project_item = server.projects.create(project_item)
+        print('Created a new project called: %s' % project_item.name)
+    except TSC.ServerResponseError:
+        print('We have already created this resource: %s' % project_item.name)
+        # raise Exception('We have already created this resource: %s' % project_item.name)
+        sys.exit()
+    return project_item
+
+def main():
+    parser = argparse.ArgumentParser(description='Get all of the refresh tasks available on a server')
+    parser.add_argument('--server', '-s', required=True, help='server address')
+    parser.add_argument('--username', '-u', required=True, help='username to sign into server')
+    parser.add_argument('--site', '-S', default=None)
+    parser.add_argument('-p', default=None)
+
+    parser.add_argument('--logging-level', '-l', choices=['debug', 'info', 'error'], default='error',
+                        help='desired logging level (set to error by default)')
+
+    args = parser.parse_args()
+
+    if args.p is None:
+        password = getpass.getpass("Password: ")
+    else:
+        password = args.p
+
+    # Set logging level based on user input, or error by default
+    logging_level = getattr(logging, args.logging_level.upper())
+    logging.basicConfig(level=logging_level)
+
+    tableau_auth = TSC.TableauAuth(args.username, password)
+    server = TSC.Server(args.server)
+
+    with server.auth.sign_in(tableau_auth):
+        # Use highest Server REST API version available
+        server.use_server_version()
+
+        top_level_project = TSC.ProjectItem(name='Top Level Project')
+        top_level_project = create_project(server, top_level_project)
+
+        child_project = TSC.ProjectItem(name='Child Project', parent_id=top_level_project.id)
+        child_project = create_project(server, child_project)
+
+        grand_child_project = TSC.ProjectItem(name='Grand Child Project', parent_id=child_project.id)
+        grand_child_project = create_project(server, grand_child_project)
+
+if __name__ == '__main__':
+    main()
