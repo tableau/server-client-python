471e796c80a30f8cd5be79e93e69ab58f9a8d5c2
Russell Hay <me@russellhay.com>
2017-09-12 14:54:05 -0700

    move views to use dynamic namespace

--- tableauserverclient/models/task_item.py
+++ tableauserverclient/models/task_item.py
@@ -18,22 +18,22 @@ class TaskItem(object):
                "schedule_id}) target({target})>".format(**self.__dict__)
 
     @classmethod
-    def from_response(cls, xml):
+    def from_response(cls, xml, ns):
         parsed_response = ET.fromstring(xml)
         all_tasks_xml = parsed_response.findall(
-            './/t:task/t:extractRefresh', namespaces=NAMESPACE)
+            './/t:task/t:extractRefresh', namespaces=ns)
 
-        all_tasks = (TaskItem._parse_element(x) for x in all_tasks_xml)
+        all_tasks = (TaskItem._parse_element(x, ns) for x in all_tasks_xml)
 
         return list(all_tasks)
 
     @classmethod
-    def _parse_element(cls, element):
+    def _parse_element(cls, element, ns):
         schedule = None
         target = None
-        schedule_element = element.find('.//t:schedule', namespaces=NAMESPACE)
-        workbook_element = element.find('.//t:workbook', namespaces=NAMESPACE)
-        datasource_element = element.find('.//t:datasource', namespaces=NAMESPACE)
+        schedule_element = element.find('.//t:schedule', namespaces=ns)
+        workbook_element = element.find('.//t:workbook', namespaces=ns)
+        datasource_element = element.find('.//t:datasource', namespaces=ns)
         if schedule_element is not None:
             schedule = schedule_element.get('id', None)
 
