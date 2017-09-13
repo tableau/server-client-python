d22d8e79d48937f16ecd7f11e6927dc24b034d7a
Russell Hay <me@russellhay.com>
2017-09-13 11:31:14 -0700

    Move detection into a Namespace class

--- tableauserverclient/namespace.py
+++ tableauserverclient/namespace.py
@@ -3,6 +3,31 @@ NEW_NAMESPACE = 'http://tableau.com/api'
 
 NAMESPACE = None
 
+class Namespace(object):
+    def __init__(self):
+        self._namespace = {'t': NEW_NAMESPACE}
+        self._detected = False
+
+    def __call__(self):
+        return self._namespace
+
+    def detect(self, xml):
+        if self._detected:
+            return
+
+        lines = (n for n in xml.split('\n') if n.startswith('<tsResponse'))
+
+        for n in lines:
+            pieces = (p for p in n.split('xmlns') if p.startswith('='))
+            for piece in pieces:
+                ns = piece.replace('=', '').replace('"', '').strip()
+                if ns == OLD_NAMESPACE:
+                    self._namespace = {'t': OLD_NAMESPACE}
+                    self._detected = True
+                elif ns == NEW_NAMESPACE:
+                    self._namespace = {'t': NEW_NAMESPACE}
+                    self._detected = True
+
 
 def namespace():
     return NAMESPACE
@@ -13,13 +38,6 @@ def set_namespace_from_xml(raw_response):
     if NAMESPACE:
         return
 
-    lines = (n for n in raw_response.split('\n') if n.startswith('<tsResponse'))
-
-    for n in lines:
-        pieces = (p for p in n.split('xmlns') if p.startswith('='))
-        for piece in pieces:
-            ns = piece.replace('=', '').replace('"', '').strip()
-            if ns == OLD_NAMESPACE:
-                NAMESPACE = {'t': OLD_NAMESPACE}
-            elif ns == NEW_NAMESPACE:
-                NAMESPACE = {'t': NEW_NAMESPACE}
+    ns = Namespace()
+    ns.detect(raw_response)
+    NAMESPACE = ns()
