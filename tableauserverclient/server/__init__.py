e18a5286fde7adbc1816712f4e3ad509f761bdbb
Russell Hay <me@russellhay.com>
2017-09-14 08:53:47 -0700

    removing unneeded import

--- tableauserverclient/server/__init__.py
+++ tableauserverclient/server/__init__.py
@@ -4,7 +4,7 @@ from .filter import Filter
 from .sort import Sort
 from .. import ConnectionItem, DatasourceItem,\
     GroupItem, PaginationItem, ProjectItem, ScheduleItem, SiteItem, TableauAuth,\
-    UserItem, ViewItem, WorkbookItem, TaskItem, namespace
+    UserItem, ViewItem, WorkbookItem, TaskItem
 from .endpoint import Auth, Datasources, Endpoint, Groups, Projects, Schedules, \
     Sites, Users, Views, Workbooks, ServerResponseError, MissingRequiredFieldError
 from .server import Server
