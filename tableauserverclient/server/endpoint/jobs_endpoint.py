022b3030565b318c62feb3f9d3b4e1fc92e6a73b
Russell Hay <russell.hay@gmail.com>
2018-05-31 08:41:21 -0700

    fixing whitespace issues

--- tableauserverclient/server/endpoint/jobs_endpoint.py
+++ tableauserverclient/server/endpoint/jobs_endpoint.py
@@ -20,7 +20,7 @@ class Jobs(Endpoint):
             return self.get_by_id(job_id)
         if isinstance(job_id, RequestOptionsBase):
             req_options = job_id
-            
+
         self.parent_srv.assert_at_least_version('3.1')
         server_response = self.get_request(self.baseurl, req_options)
         pagination_item = PaginationItem.from_response(server_response.content, self.parent_srv.namespace)
