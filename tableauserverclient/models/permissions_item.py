966b28fe50312260be48e005a11af2f9d037b91d
Sam Luen-English <sam.luenenglish@atheon.co.uk>
2019-07-12 10:42:13 +0100

    Refactor permission items

--- tableauserverclient/models/permissions_item.py
+++ tableauserverclient/models/permissions_item.py
@@ -4,117 +4,114 @@ import logging
 from .exceptions import UnknownGranteeTypeError
 
 
-logger = logging.getLogger('tableau.models.permissions_item')
+logger = logging.getLogger("tableau.models.permissions_item")
 
 
 class Permission:
     class GranteeType:
-        User = 'user'
-        Group = 'group'
+        User = "user"
+        Group = "group"
 
     class CapabilityMode:
-        Allow = 'Allow'
-        Deny = 'Deny'
+        Allow = "Allow"
+        Deny = "Deny"
 
     class DatasourceCapabilityType:
-        ChangePermissions = 'ChangePermissions'
-        Connect = 'Connect'
-        Delete = 'Delete'
-        ExportXml = 'ExportXml'
-        Read = 'Read'
-        Write = 'Write'
+        ChangePermissions = "ChangePermissions"
+        Connect = "Connect"
+        Delete = "Delete"
+        ExportXml = "ExportXml"
+        Read = "Read"
+        Write = "Write"
 
     class WorkbookCapabilityType:
-        AddComment = 'AddComment'
-        ChangeHierarchy = 'ChangeHierarchy'
-        ChangePermissions = 'ChangePermissions'
-        Delete = 'Delete'
-        ExportData = 'ExportData'
-        ExportImage = 'ExportImage'
-        ExportXml = 'ExportXml'
-        Filter = 'Filter'
-        Read = 'Read'
-        ShareView = 'ShareView'
-        ViewComments = 'ViewComments'
-        ViewUnderlyingData = 'ViewUnderlyingData'
-        WebAuthoring = 'WebAuthoring'
-        Write = 'Write'
+        AddComment = "AddComment"
+        ChangeHierarchy = "ChangeHierarchy"
+        ChangePermissions = "ChangePermissions"
+        Delete = "Delete"
+        ExportData = "ExportData"
+        ExportImage = "ExportImage"
+        ExportXml = "ExportXml"
+        Filter = "Filter"
+        Read = "Read"
+        ShareView = "ShareView"
+        ViewComments = "ViewComments"
+        ViewUnderlyingData = "ViewUnderlyingData"
+        WebAuthoring = "WebAuthoring"
+        Write = "Write"
 
     class ProjectCapabilityType:
-        ProjectLeader = 'ProjectLeader'
-        Read = 'Read'
-        Write = 'Write'
+        ProjectLeader = "ProjectLeader"
+        Read = "Read"
+        Write = "Write"
+
+
+class PermissionsGrantee(object):
+    def __init__(self, grantee_type, grantee_id):
+
+        if grantee_type not in [Permission.GranteeType.User, Permission.GranteeType.Group]:
+            raise UnknownGranteeTypeError()
+
+        self._grantee_type = grantee_type
+        self._grantee_id = grantee_id
+
+    @classmethod
+    def from_xml_element(cls, xml_element):
+        return cls(xml_element.tag, xml_element.get("id"))
+
+    def to_xml_element(self):
+        xml_element = ET.Element(self.grantee_type)
+        xml_element.set("id", self.grantee_id)
+        return xml_element
+
+    @property
+    def grantee_type(self):
+        return self._grantee_type
+
+    @property
+    def grantee_id(self):
+        return self._grantee_id
 
 
 class PermissionsRule(object):
-    def __init__(self, type=None, object_id=None, map={}):
-        self._type = type
-        self._object_id = object_id
-        self.map = map
+    def __init__(self, grantee, permissions_map=None):
+        self._grantee = grantee
+        self.permissions_map = permissions_map or {}
 
     @property
-    def type(self):
-        return self._type
-
-    @property
-    def object_id(self):
-        return self._object_id
+    def grantee(self):
+        return self._grantee
 
 
 class PermissionsCollection(object):
-    def __init__(self):
-        self._capabilities = None
-
-    def _set_values(self, capabilities):
-        self._capabilities = capabilities
+    def __init__(self, rules):
+        self._rules = rules
 
     @property
-    def capabilities(self):
-        return self._capabilities
+    def rules(self):
+        return self._rules
 
     @classmethod
     def from_response(cls, resp, ns=None):
-        permissions = PermissionsCollection()
         parsed_response = ET.fromstring(resp)
 
-        capabilities = {}
-        all_xml = parsed_response.findall('.//t:granteeCapabilities',
-                                          namespaces=ns)
+        capabilities = []
+        all_xml = parsed_response.findall(".//t:granteeCapabilities", namespaces=ns)
 
         for grantee_capability_xml in all_xml:
-            grantee_id = None
-            grantee_type = None
-            capability_map = {}
 
-            try:
-                grantee_id = grantee_capability_xml.find('.//t:group',
-                                                         namespaces=ns)\
-                                                    .get('id')
-                grantee_type = Permission.GranteeType.Group
-            except AttributeError:
-                pass
-            try:
-                grantee_id = grantee_capability_xml.find('.//t:user',
-                                                         namespaces=ns)\
-                                                    .get('id')
-                grantee_type = Permission.GranteeType.User
-            except AttributeError:
-                pass
+            grantee_element = grantee_capability_xml[0]
+            grantee = PermissionsGrantee.from_xml_element(grantee_element)
 
-            if grantee_id is None:
-                logger.error('Cannot find grantee type in response')
-                raise UnknownGranteeTypeError()
+            capability_elements = grantee_capability_xml.findall(
+                ".//t:capabilities/t:capability", namespaces=ns
+            )
+            capability_map = {
+                el.get("name"): el.get("mode") for el in capability_elements
+            }
 
-            for capability_xml in grantee_capability_xml.findall(
-                    './/t:capabilities/t:capability', namespaces=ns):
-                name = capability_xml.get('name')
-                mode = capability_xml.get('mode')
+            capability_item = PermissionsRule(grantee, capability_map)
 
-                capability_map[name] = mode
+            capabilities.append(capability_item)
 
-            capability_item = PermissionsRule(grantee_type, grantee_id,
-                                              capability_map)
-            capabilities[(grantee_type, grantee_id)] = capability_item
-
-        permissions._set_values(capabilities)
-        return permissions
+        return cls(capabilities)
