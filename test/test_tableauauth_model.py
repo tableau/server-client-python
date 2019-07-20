bfe9de761fee15cb2f40bd21e5188bccaaae9627
workgroupuser <HTTP/qa-centos.tsi.lan@tsi.lan>
2019-07-20 00:19:35 +0000

    removing test because it doesn't make sense in the new context

--- test/test_tableauauth_model.py
+++ test/test_tableauauth_model.py
@@ -10,10 +10,6 @@ class TableauAuthModelTests(unittest.TestCase):
                                     site_id='site1',
                                     user_id_to_impersonate='admin')
 
-    def test_username_password_required(self):
-        with self.assertRaises(TypeError):
-            TSC.TableauAuth()
-
     def test_site_arg_raises_warning(self):
         with warnings.catch_warnings(record=True) as w:
             warnings.simplefilter("always")
