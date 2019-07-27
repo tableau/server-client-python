bfbd864ebae6813e19a3dfa9ff8b41dbdf387f11
Tyler Doyle <kingt8y8@gmail.com>
2019-07-27 00:20:16 -0700

    merge conflict 2

--- tableauserverclient/__init__.py
+++ tableauserverclient/__init__.py
@@ -1,7 +1,7 @@
 from .namespace import NEW_NAMESPACE as DEFAULT_NAMESPACE
 from .models import ConnectionCredentials, ConnectionItem, DatasourceItem,\
     GroupItem, JobItem, BackgroundJobItem, PaginationItem, ProjectItem, ScheduleItem,\
-    SiteItem, TableauAuth, UserItem, ViewItem, WorkbookItem, UnpopulatedPropertyError,\
+    SiteItem, TableauAuth, PersonalAccessTokenAuth, UserItem, ViewItem, WorkbookItem, UnpopulatedPropertyError,\
     HourlyInterval, DailyInterval, WeeklyInterval, MonthlyInterval, IntervalItem, TaskItem,\
     SubscriptionItem, Target, PermissionsRule, Permission, DatabaseItem, TableItem, ColumnItem
 from .server import RequestOptions, CSVRequestOptions, ImageRequestOptions, PDFRequestOptions, Filter, Sort, \
