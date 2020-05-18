bd9d32b2717ee9d522b63784cc9c33958c9df22b
Tyler Doyle <kingt8y8@gmail.com>
2020-05-18 14:00:34 -0700

    Simple paging endpoint for graphql

--- tableauserverclient/server/endpoint/metadata_endpoint.py
+++ tableauserverclient/server/endpoint/metadata_endpoint.py
@@ -1,36 +1,42 @@
 from .endpoint import Endpoint, api
-from .exceptions import GraphQLError
+from .exceptions import GraphQLError, InvalidGraphQLQuery
 import logging
 import json
 
 logger = logging.getLogger('tableau.endpoint.metadata')
 
 
+def is_valid_paged_query(parsed_query):
+    """Check that the required $first and $afterToken variables are present in the query"""
+    return all(k in parsed_query['variables'] for k in ('first', 'afterToken'))
+
+
+def extract_values(obj, key):
+    """Pull all values of specified key from nested JSON.
+    Taken from: https://hackersandslackers.com/extract-data-from-complex-json-python/"""
+    arr = []
+
+    def extract(obj, arr, key):
+        """Recursively search for values of key in JSON tree."""
+        if isinstance(obj, dict):
+            for k, v in obj.items():
+                if isinstance(v, (dict, list)):
+                    extract(v, arr, key)
+                elif k == key:
+                    arr.append(v)
+        elif isinstance(obj, list):
+            for item in obj:
+                extract(item, arr, key)
+        return arr
+
+    results = extract(obj, arr, key)
+    return results
+
+
 class Metadata(Endpoint):
     @property
     def baseurl(self):
         return "{0}/api/metadata/graphql".format(self.parent_srv.server_address)
-
-    @staticmethod
-    def extract_values(obj, key):
-        """Pull all values of specified key from nested JSON."""
-        arr = []
-
-        def extract(obj, arr, key):
-            """Recursively search for values of key in JSON tree."""
-            if isinstance(obj, dict):
-                for k, v in obj.items():
-                    if isinstance(v, (dict, list)):
-                        extract(v, arr, key)
-                    elif k == key:
-                        arr.append(v)
-            elif isinstance(obj, list):
-                for item in obj:
-                    extract(item, arr, key)
-            return arr
-
-        results = extract(obj, arr, key)
-        return results
 
     @staticmethod
     def get_page_info(result):
@@ -45,9 +51,8 @@ class Metadata(Endpoint):
 
         try:
             graphql_query = json.dumps({'query': query, 'variables': variables})
-        except Exception:
-            # Place holder for now
-            raise Exception('Must provide a string')
+        except Exception as e:
+            raise InvalidGraphQLQuery('Must provide a string')
 
         # Setting content type because post_reuqest defaults to text/xml
         server_response = self.post_request(url, graphql_query, content_type='text/json')
@@ -60,36 +65,35 @@ class Metadata(Endpoint):
 
     @api("3.2")
     def paginated_query(self, query, variables=None, abort_on_error=False):
-        #boilerplate
-        logger.info('Querying Metadata API')
+        logger.info('Querying Metadata API using a Paged Query')
         url = self.baseurl
-        # validate this is a pagable query
-        # if all (k in foo for k in ("foo","bar"))
 
         graphql_query = json.dumps({'query': query, 'variables': variables})
         parsed_query = json.loads(graphql_query)
 
-        if not all(k in parsed_query['variables'] for k in ('first', 'afterToken')):
-            raise Exception
+        if not is_valid_paged_query(parsed_query):
+            raise InvalidGraphQLQuery('Paged queries must have a $first and $afterToken variable')
 
         paginated_results = []
 
         # get first page
         server_response = self.post_request(url, graphql_query, content_type='text/json')
         results = server_response.json()
-        
+
         if abort_on_error and results.get('errors', None):
             raise GraphQLError(results['errors'])
-        
+
         paginated_results.append(results)
         # repeat
         has_another_page, cursor = self.get_page_info(results)
         while has_another_page:
             variables.update({'afterToken': cursor})
-            print("Calling Token: " + cursor)
+            logger.debug("Calling Token: " + cursor)
             graphql_query = json.dumps({'query': query, 'variables': variables})
             has_another_page, cursor = self.get_page_info(results)
             server_response = self.post_request(url, graphql_query, content_type='text/json')
             results = server_response.json()
             paginated_results.append(results)
+
+        logger.info('Sucessfully got results for paged query')
         return paginated_results
