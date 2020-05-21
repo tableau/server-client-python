60e7b72694bccb8ed8701d4d39af0eeaf444d070
Tyler Doyle <kingt8y8@gmail.com>
2020-05-20 18:57:41 -0700

    fix many little bugs and add tests

--- tableauserverclient/server/endpoint/metadata_endpoint.py
+++ tableauserverclient/server/endpoint/metadata_endpoint.py
@@ -7,8 +7,13 @@ logger = logging.getLogger('tableau.endpoint.metadata')
 
 
 def is_valid_paged_query(parsed_query):
-    """Check that the required $first and $afterToken variables are present in the query"""
-    return all(k in parsed_query['variables'] for k in ('first', 'afterToken'))
+    """Check that the required $first and $afterToken variables are present in the query.
+    Also check that we are asking for the pageInfo object, so we get the endCursor. There
+    is no way to do this relilably without writing a GraphQL parser, so simply check that
+    that the string contains 'hasNextPage' and 'endCursor'"""
+    return all(k in parsed_query['variables'] for k in ('first', 'afterToken')) and \
+        'hasNextPage' in parsed_query['query'] and \
+        'endCursor' in parsed_query['query']
 
 
 def extract_values(obj, key):
@@ -32,10 +37,11 @@ def extract_values(obj, key):
     results = extract(obj, arr, key)
     return results
 
-    def get_page_info(result):
-        next_page = extract_values(result, 'hasNextPage').pop()
-        cursor = extract_values(result, 'endCursor').pop()
-        return next_page, cursor
+
+def get_page_info(result):
+    next_page = extract_values(result, 'hasNextPage').pop()
+    cursor = extract_values(result, 'endCursor').pop()
+    return next_page, cursor
 
 
 class Metadata(Endpoint):
@@ -67,13 +73,22 @@ class Metadata(Endpoint):
         logger.info('Querying Metadata API using a Paged Query')
         url = self.baseurl
 
+        if variables is None:
+            # default paramaters
+            variables = {'first': 100, 'afterToken': None}
+        elif (('first' in variables) and ('afterToken' not in variables)):
+            # they passed a page size but not a token, probably because they're starting at `null` token
+            variables.update({'afterToken': None})
+
         graphql_query = json.dumps({'query': query, 'variables': variables})
         parsed_query = json.loads(graphql_query)
 
         if not is_valid_paged_query(parsed_query):
-            raise InvalidGraphQLQuery('Paged queries must have a $first and $afterToken variable')
+            raise InvalidGraphQLQuery('Paged queries must have a `$first` and `$afterToken` variables as well as '
+                                      'a pageInfo object with `endCursor` and `hasNextPage`')
 
-        paginated_results = []
+        results_dict = {'pages': []}
+        paginated_results = results_dict['pages']
 
         # get first page
         server_response = self.post_request(url, graphql_query, content_type='text/json')
@@ -83,16 +98,18 @@ class Metadata(Endpoint):
             raise GraphQLError(results['errors'])
 
         paginated_results.append(results)
+
         # repeat
         has_another_page, cursor = get_page_info(results)
+
         while has_another_page:
             variables.update({'afterToken': cursor})
             logger.debug("Calling Token: " + cursor)
             graphql_query = json.dumps({'query': query, 'variables': variables})
-            has_another_page, cursor = get_page_info(results)
             server_response = self.post_request(url, graphql_query, content_type='text/json')
             results = server_response.json()
             paginated_results.append(results)
+            has_another_page, cursor = get_page_info(results)
 
         logger.info('Sucessfully got results for paged query')
-        return paginated_results
+        return results_dict
--- test/test_metadata.py
+++ test/test_metadata.py
@@ -10,6 +10,11 @@ TEST_ASSET_DIR = os.path.join(os.path.dirname(__file__), 'assets')
 
 METADATA_QUERY_SUCCESS = os.path.join(TEST_ASSET_DIR, 'metadata_query_success.json')
 METADATA_QUERY_ERROR = os.path.join(TEST_ASSET_DIR, 'metadata_query_error.json')
+EXPECTED_PAGED_DICT = os.path.join(TEST_ASSET_DIR, 'metadata_query_expected_dict.dict')
+
+METADATA_PAGE_1 = os.path.join(TEST_ASSET_DIR, 'metadata_paged_1.json')
+METADATA_PAGE_2 = os.path.join(TEST_ASSET_DIR, 'metadata_paged_2.json')
+METADATA_PAGE_3 = os.path.join(TEST_ASSET_DIR, 'metadata_paged_3.json')
 
 EXPECTED_DICT = {'publishedDatasources':
                  [{'id': '01cf92b2-2d17-b656-fc48-5c25ef6d5352', 'name': 'Batters (TestV1)'},
@@ -45,6 +50,30 @@ class MetadataTests(unittest.TestCase):
             datasources = actual['data']
 
         self.assertDictEqual(EXPECTED_DICT, datasources)
+
+    def test_paged_metadata_query(self):
+        with open(EXPECTED_PAGED_DICT, 'rb') as f:
+            expected = eval(f.read())
+
+        # prepare the 3 pages of results
+        with open(METADATA_PAGE_1, 'rb') as f:
+            result_1 = f.read().decode()
+        with open(METADATA_PAGE_2, 'rb') as f:
+            result_2 = f.read().decode()
+        with open(METADATA_PAGE_3, 'rb') as f:
+            result_3 = f.read().decode()
+
+        with requests_mock.mock() as m:
+            m.post(self.baseurl, [{'text': result_1, 'status_code': 200},
+                                  {'text': result_2, 'status_code': 200},
+                                  {'text': result_3, 'status_code': 200}])
+
+            # validation checks for endCursor and hasNextPage,
+            # but the query text doesn't matter for the test
+            actual = self.server.metadata.paginated_query('fake query endCursor hasNextPage',
+                                                          variables={'first': 1, 'afterToken': None})
+
+        self.assertDictEqual(expected, actual)
 
     def test_metadata_query_ignore_error(self):
         with open(METADATA_QUERY_ERROR, 'rb') as f:
