c9d7c6a78f529c4f5e2021a8aa53607dad15d3b4
Chris Shin <cshin@tableau.com>
2020-10-16 10:04:24 -0700

    Updates datasource_item with new fields (#705)
    * Adds new fields and updates parser
    
    * Adds new fields to publish and update requests
    
    * Adds tests and description field

--- tableauserverclient/models/datasource_item.py
+++ tableauserverclient/models/datasource_item.py
@@ -1,29 +1,49 @@
 import xml.etree.ElementTree as ET
 from .exceptions import UnpopulatedPropertyError
-from .property_decorators import property_not_nullable, property_is_boolean
+from .property_decorators import property_not_nullable, property_is_boolean, property_is_enum
 from .tag_item import TagItem
 from ..datetime_helpers import parse_datetime
 import copy
 
 
 class DatasourceItem(object):
+    class AskDataEnablement:
+        Enabled = 'Enabled'
+        Disabled = 'Disabled'
+        SiteDefault = 'SiteDefault'
+
     def __init__(self, project_id, name=None):
+        self._ask_data_enablement = None
+        self._certified = None
+        self._certification_note = None
         self._connections = None
         self._content_url = None
         self._created_at = None
         self._datasource_type = None
+        self._encrypt_extracts = None
+        self._has_extracts = None
         self._id = None
         self._initial_tags = set()
         self._project_name = None
         self._updated_at = None
-        self._certified = None
-        self._certification_note = None
+        self._use_remote_query_agent = None
+        self._webpage_url = None
+        self.description = None
         self.name = name
         self.owner_id = None
         self.project_id = project_id
         self.tags = set()
 
         self._permissions = None
+
+    @property
+    def ask_data_enablement(self):
+        return self._ask_data_enablement
+
+    @ask_data_enablement.setter
+    @property_is_enum(AskDataEnablement)
+    def ask_data_enablement(self, value):
+        self._ask_data_enablement = value
 
     @property
     def connections(self):
@@ -66,6 +86,15 @@ class DatasourceItem(object):
         self._certification_note = value
 
     @property
+    def encrypt_extracts(self):
+        return self._encrypt_extracts
+
+    @encrypt_extracts.setter
+    @property_is_boolean
+    def encrypt_extracts(self, value):
+        self._encrypt_extracts = value
+
+    @property
     def id(self):
         return self._id
 
@@ -90,6 +119,15 @@ class DatasourceItem(object):
     def updated_at(self):
         return self._updated_at
 
+    @property
+    def use_remote_query_agent(self):
+        return self._use_remote_query_agent
+
+    @use_remote_query_agent.setter
+    @property_is_boolean
+    def use_remote_query_agent(self, value):
+        self._use_remote_query_agent = value
+
     def _set_connections(self, connections):
         self._connections = connections
 
@@ -100,38 +138,53 @@ class DatasourceItem(object):
         if not isinstance(datasource_xml, ET.Element):
             datasource_xml = ET.fromstring(datasource_xml).find('.//t:datasource', namespaces=ns)
         if datasource_xml is not None:
-            (_, _, _, _, _, updated_at, _, project_id, project_name, owner_id,
-             certified, certification_note) = self._parse_element(datasource_xml, ns)
-            self._set_values(None, None, None, None, None, updated_at, None, project_id,
-                             project_name, owner_id, certified, certification_note)
+            (ask_data_enablement, certified, certification_note, _, _, _, _, encrypt_extracts, has_extracts,
+             _, _, owner_id, project_id, project_name, _, updated_at, use_remote_query_agent,
+             webpage_url) = self._parse_element(datasource_xml, ns)
+            self._set_values(ask_data_enablement, certified, certification_note, None, None, None, None,
+                             encrypt_extracts, has_extracts, None, None, owner_id, project_id, project_name, None,
+                             updated_at, use_remote_query_agent, webpage_url)
         return self
 
-    def _set_values(self, id, name, datasource_type, content_url, created_at,
-                    updated_at, tags, project_id, project_name, owner_id, certified, certification_note):
-        if id is not None:
-            self._id = id
-        if name:
-            self.name = name
-        if datasource_type:
-            self._datasource_type = datasource_type
+    def _set_values(self, ask_data_enablement, certified, certification_note, content_url, created_at, datasource_type,
+                    description, encrypt_extracts, has_extracts, id_, name, owner_id, project_id, project_name, tags,
+                    updated_at, use_remote_query_agent, webpage_url):
+        if ask_data_enablement is not None:
+            self._ask_data_enablement = ask_data_enablement
+        if certification_note:
+            self.certification_note = certification_note
+        self.certified = certified  # Always True/False, not conditional
         if content_url:
             self._content_url = content_url
         if created_at:
             self._created_at = created_at
-        if updated_at:
-            self._updated_at = updated_at
-        if tags:
-            self.tags = tags
-            self._initial_tags = copy.copy(tags)
+        if datasource_type:
+            self._datasource_type = datasource_type
+        if description:
+            self.description = description
+        if encrypt_extracts is not None:
+            self.encrypt_extracts = str(encrypt_extracts).lower() == 'true'
+        if has_extracts is not None:
+            self._has_extracts = str(has_extracts).lower() == 'true'
+        if id_ is not None:
+            self._id = id_
+        if name:
+            self.name = name
+        if owner_id:
+            self.owner_id = owner_id
         if project_id:
             self.project_id = project_id
         if project_name:
             self._project_name = project_name
-        if owner_id:
-            self.owner_id = owner_id
-        if certification_note:
-            self.certification_note = certification_note
-        self.certified = certified  # Always True/False, not conditional
+        if tags:
+            self.tags = tags
+            self._initial_tags = copy.copy(tags)
+        if updated_at:
+            self._updated_at = updated_at
+        if use_remote_query_agent is not None:
+            self._use_remote_query_agent = str(use_remote_query_agent).lower() == 'true'
+        if webpage_url:
+            self._webpage_url = webpage_url
 
     @classmethod
     def from_response(cls, resp, ns):
@@ -140,25 +193,32 @@ class DatasourceItem(object):
         all_datasource_xml = parsed_response.findall('.//t:datasource', namespaces=ns)
 
         for datasource_xml in all_datasource_xml:
-            (id_, name, datasource_type, content_url, created_at, updated_at,
-             tags, project_id, project_name, owner_id,
-             certified, certification_note) = cls._parse_element(datasource_xml, ns)
+            (ask_data_enablement, certified, certification_note, content_url, created_at, datasource_type,
+             description, encrypt_extracts, has_extracts, id_, name, owner_id, project_id, project_name, tags,
+             updated_at, use_remote_query_agent, webpage_url) = cls._parse_element(datasource_xml, ns)
             datasource_item = cls(project_id)
-            datasource_item._set_values(id_, name, datasource_type, content_url, created_at, updated_at,
-                                        tags, None, project_name, owner_id, certified, certification_note)
+            datasource_item._set_values(ask_data_enablement, certified, certification_note, content_url,
+                                        created_at, datasource_type, description, encrypt_extracts,
+                                        has_extracts, id_, name, owner_id, None, project_name, tags, updated_at,
+                                        use_remote_query_agent, webpage_url)
             all_datasource_items.append(datasource_item)
         return all_datasource_items
 
     @staticmethod
     def _parse_element(datasource_xml, ns):
-        id_ = datasource_xml.get('id', None)
-        name = datasource_xml.get('name', None)
-        datasource_type = datasource_xml.get('type', None)
-        content_url = datasource_xml.get('contentUrl', None)
-        created_at = parse_datetime(datasource_xml.get('createdAt', None))
-        updated_at = parse_datetime(datasource_xml.get('updatedAt', None))
         certification_note = datasource_xml.get('certificationNote', None)
         certified = str(datasource_xml.get('isCertified', None)).lower() == 'true'
+        content_url = datasource_xml.get('contentUrl', None)
+        created_at = parse_datetime(datasource_xml.get('createdAt', None))
+        datasource_type = datasource_xml.get('type', None)
+        description = datasource_xml.get('description', None)
+        encrypt_extracts = datasource_xml.get('encryptExtracts', None)
+        has_extracts = datasource_xml.get('hasExtracts', None)
+        id_ = datasource_xml.get('id', None)
+        name = datasource_xml.get('name', None)
+        updated_at = parse_datetime(datasource_xml.get('updatedAt', None))
+        use_remote_query_agent = datasource_xml.get('useRemoteQueryAgent', None)
+        webpage_url = datasource_xml.get('webpageUrl', None)
 
         tags = None
         tags_elem = datasource_xml.find('.//t:tags', namespaces=ns)
@@ -177,5 +237,11 @@ class DatasourceItem(object):
         if owner_elem is not None:
             owner_id = owner_elem.get('id', None)
 
-        return (id_, name, datasource_type, content_url, created_at, updated_at, tags, project_id,
-                project_name, owner_id, certified, certification_note)
+        ask_data_enablement = None
+        ask_data_elem = datasource_xml.find('.//t:askData', namespaces=ns)
+        if ask_data_elem is not None:
+            ask_data_enablement = ask_data_elem.get('enablement', None)
+
+        return (ask_data_enablement, certified, certification_note, content_url, created_at,
+                datasource_type, description, encrypt_extracts, has_extracts, id_, name, owner_id,
+                project_id, project_name, tags, updated_at, use_remote_query_agent, webpage_url)
