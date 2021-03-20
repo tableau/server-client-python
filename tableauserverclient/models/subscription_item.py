90fc0b455b51b86b7ac451b2895231cf574edd86
Jordan Woods <Jordan.Woods@mkcorp.com>
2021-03-20 14:30:00 -0500

    Fix repr on subscription item

--- tableauserverclient/models/subscription_item.py
+++ tableauserverclient/models/subscription_item.py
@@ -21,11 +21,11 @@ class SubscriptionItem(object):
 
     def __repr__(self):
         if self.id is not None:
-            return "<Subscription#{id} subject({subject}) schedule_id({schedule_id}) user_id({user_id}) \
-                target({target})".format(**self.__dict__)
+            return ("<Subscription#{_id} subject({subject}) schedule_id({schedule_id}) user_id({user_id}) "
+                    "target({target})").format(**self.__dict__)
         else:
-            return "<Subscription subject({subject}) schedule_id({schedule_id}) user_id({user_id}) \
-                target({target})".format(**self.__dict__)
+            return ("<Subscription subject({subject}) schedule_id({schedule_id}) user_id({user_id}) "
+                    "target({target})").format(**self.__dict__)
 
     @property
     def id(self):
