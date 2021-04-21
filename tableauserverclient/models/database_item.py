e666ee67eecb4fc238ad846ff108a28c47a1f12d
t8y8 <doyle.tyler@gmail.com>
2021-04-21 12:08:09 -0700

    checkpoint 5 - cleanup

--- tableauserverclient/models/database_item.py
+++ tableauserverclient/models/database_item.py
@@ -251,8 +251,8 @@ class DatabaseItem(object):
     def _set_data_quality_warnings(self, dqw):
         self._data_quality_warnings = dqw
 
-    def _set_dqws(self, dqw):
-        self._dqws = dqw
+    def _set_data_quality_warnings(self, dqw):
+        self._data_quality_warningss = dqw
 
     @classmethod
     def from_response(cls, resp, ns):
