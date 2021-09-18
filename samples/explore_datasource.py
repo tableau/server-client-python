74bec027ca1c3975b73ad88415643c761da3f16b
Adrian Vogelsgesang <avogelsgesang@tableau.com>
2021-09-18 14:30:19 +0200

    Unify arguments of sample scripts
    I am pretty new to TSC, and wanted to run some sample scripts to get an
    understanding of the library. Doing so, I realized that every sample
    had a slightly different command line, even for common arguments:
    * Some expected `site`, some `site-id`, some were lacking site-support
      completely (and thereby unusable for Tableau Online)
    * Some had a short option `-i`, some had the short option `-S` for the
      site name
    * Some expected password-based authentication, some expected personal
      access tokens
    
    This commit fixes all those inconsistencies, so that users don't have
    to re-learn the command line options for each individual script.

--- samples/explore_datasource.py
+++ samples/explore_datasource.py
@@ -10,7 +10,6 @@
 ####
 
 import argparse
-import getpass
 import logging
 
 import tableauserverclient as TSC
@@ -19,25 +18,28 @@ import tableauserverclient as TSC
 def main():
 
     parser = argparse.ArgumentParser(description='Explore datasource functions supported by the Server API.')
+    # Common options; please keep those in sync across all samples
     parser.add_argument('--server', '-s', required=True, help='server address')
-    parser.add_argument('--username', '-u', required=True, help='username to sign into server')
-    parser.add_argument('--publish', '-p', metavar='FILEPATH', help='path to datasource to publish')
-    parser.add_argument('--download', '-d', metavar='FILEPATH', help='path to save downloaded datasource')
+    parser.add_argument('--site', '-S', help='site name')
+    parser.add_argument('--token-name', '-p', required=True,
+                        help='name of the personal access token used to sign into the server')
+    parser.add_argument('--token-value', '-v', required=True,
+                        help='value of the personal access token used to sign into the server')
     parser.add_argument('--logging-level', '-l', choices=['debug', 'info', 'error'], default='error',
                         help='desired logging level (set to error by default)')
+    # Options specific to this sample
+    parser.add_argument('--publish', metavar='FILEPATH', help='path to datasource to publish')
+    parser.add_argument('--download', metavar='FILEPATH', help='path to save downloaded datasource')
 
     args = parser.parse_args()
-
-    password = getpass.getpass("Password: ")
 
     # Set logging level based on user input, or error by default
     logging_level = getattr(logging, args.logging_level.upper())
     logging.basicConfig(level=logging_level)
 
     # SIGN IN
-    tableau_auth = TSC.TableauAuth(args.username, password)
-    server = TSC.Server(args.server)
-    server.use_highest_version()
+    tableau_auth = TSC.PersonalAccessTokenAuth(args.token_name, args.token_value, site_id=args.site)
+    server = TSC.Server(args.server, use_server_version=True)
     with server.auth.sign_in(tableau_auth):
         # Query projects for use when demonstrating publishing and updating
         all_projects, pagination_item = server.projects.get()
