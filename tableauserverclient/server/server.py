27897f80a858955de656600992551f2d763f3f45
Jordan Woods <lenluin@gmail.com>
2021-11-10 07:48:37 -0600

    Bump default Server version

--- tableauserverclient/server/server.py
+++ tableauserverclient/server/server.py
@@ -59,7 +59,7 @@ class Server(object):
         self._session = requests.Session()
         self._http_options = dict()
 
-        self.version = "2.3"
+        self.version = "3.10"
         self.auth = Auth(self)
         self.views = Views(self)
         self.users = Users(self)
