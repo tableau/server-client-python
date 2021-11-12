8a05069dd25e3e55ea0806ce741d4e6e7acbe1b4
Jordan Woods <lenluin@gmail.com>
2021-11-12 08:45:11 -0600

    Type hint TagItem

--- tableauserverclient/models/tag_item.py
+++ tableauserverclient/models/tag_item.py
@@ -1,13 +1,14 @@
+from typing import Set
 import xml.etree.ElementTree as ET
 
 
 class TagItem(object):
     @classmethod
-    def from_response(cls, resp, ns):
+    def from_response(cls, resp: bytes, ns) -> Set[str]:
         return cls.from_xml_element(ET.fromstring(resp), ns)
 
     @classmethod
-    def from_xml_element(cls, parsed_response, ns):
+    def from_xml_element(cls, parsed_response: ET.Element, ns) -> Set[str]:
         all_tags = set()
         tag_elem = parsed_response.findall(".//t:tag", namespaces=ns)
         for tag_xml in tag_elem:
