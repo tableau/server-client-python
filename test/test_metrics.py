58ee4d3d3c45b765aa0c85da30200003957b5370
Jordan Woods <lenluin@gmail.com>
2022-02-18 09:29:38 -0600

    Fix metrics tests

--- test/test_metrics.py
+++ test/test_metrics.py
@@ -13,7 +13,7 @@ METRICS_UPDATE = assets / "metrics_update.xml"
 
 class TestMetrics(unittest.TestCase):
     def setUp(self) -> None:
-        self.server = TSC.Server("http://test")
+        self.server = TSC.Server("http://test", False)
 
         # Fake signin
         self.server._site_id = "dad65087-b08b-4603-af4e-2887b8aafc67"
