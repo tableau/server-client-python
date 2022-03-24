aaa1d83ba19a90000798bca0983b41d704dc6fe7
Jac Fitzgerald <jac.fitzgerald@salesforce.com>
2022-03-24 13:06:31 -0700

    run black on tests and samples
    also update the example command line that I always copy

--- samples/create_schedules.py
+++ samples/create_schedules.py
@@ -44,7 +44,7 @@ def main():
 
     tableau_auth = TSC.PersonalAccessTokenAuth(args.token_name, args.token_value, site_id=args.site)
     server = TSC.Server(args.server, use_server_version=False)
-    server.add_http_options({'verify':False})
+    server.add_http_options({"verify": False})
     server.use_server_version()
     with server.auth.sign_in(tableau_auth):
         # Hourly Schedule
