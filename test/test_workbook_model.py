3d81e00a8ff0f8632498c70017ca77143b1408f0
bcmyguest1 <49045013+bcmyguest1@users.noreply.github.com>
2022-05-06 23:29:09 -0400

    Workbook item nullable project (#1028)
    * make project_id nullable to support "Personal Space"

--- test/test_workbook_model.py
+++ test/test_workbook_model.py
@@ -4,12 +4,6 @@ import tableauserverclient as TSC
 
 
 class WorkbookModelTests(unittest.TestCase):
-    def test_invalid_project_id(self):
-        self.assertRaises(ValueError, TSC.WorkbookItem, None)
-        workbook = TSC.WorkbookItem("10")
-        with self.assertRaises(ValueError):
-            workbook.project_id = None
-
     def test_invalid_show_tabs(self):
         workbook = TSC.WorkbookItem("10")
         with self.assertRaises(ValueError):
