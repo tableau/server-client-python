9a4774b92c239ec350bcd0bd245e0480bd792754
Jordan Woods <lenluin@gmail.com>
2022-05-23 19:04:15 -0500

    Reorder response status evaluation

--- tableauserverclient/server/endpoint/endpoint.py
+++ tableauserverclient/server/endpoint/endpoint.py
@@ -73,15 +73,18 @@ class Endpoint(object):
 
         server_response = method(url, **parameters)
 
+        # Check if response is xml, and if so, validate namespace. Checking the
+        # content type header prevents eager evaluation of streaming requests.
+        if server_response.headers.get("Content-Type") == "application/xml":
+            self.parent_srv._namespace.detect(server_response.content)
+        self._check_status(server_response)
+
         # Response.content is a property. Calling it will load the entire response into memory. Checking if the
         # content-type is an octet-stream accomplishes the same goal without eagerly loading content.
         stream = parameters.get("stream", False)
         stream = stream or (server_response.headers.get("Content-Type") == "application/octet-stream")
         if stream:
             return server_response
-
-        self.parent_srv._namespace.detect(server_response.content)
-        self._check_status(server_response)
 
         # This check is to determine if the response is a text response (xml or otherwise)
         # so that we do not attempt to log bytes and other binary data.
