92e620cb025cf6f940a6ee08d452fdc5199428e4
Jac Fitzgerald <jac.fitzgerald@salesforce.com>
2022-08-04 17:17:18 -0700

    Add user import example in samples

--- samples/create_group.py
+++ samples/create_group.py
@@ -8,10 +8,13 @@
 
 import argparse
 import logging
+import os
 
 from datetime import time
+from typing import List
 
 import tableauserverclient as TSC
+from tableauserverclient import ServerResponseError
 
 
 def main():
@@ -35,7 +38,7 @@ def main():
     )
     # Options specific to this sample
     # This sample has no additional options, yet. If you add some, please add them here
-
+    parser.add_argument("--file", help="csv file containing user info", required=False)
     args = parser.parse_args()
 
     # Set logging level based on user input, or error by default
@@ -45,10 +48,48 @@ def main():
     tableau_auth = TSC.PersonalAccessTokenAuth(args.token_name, args.token_value, site_id=args.site)
     server = TSC.Server(args.server, use_server_version=True)
     with server.auth.sign_in(tableau_auth):
-        group = TSC.GroupItem("test")
-        group = server.groups.create(group)
-        print(group)
+        # this code shows 3 different error codes that mean "resource is already in collection"
+        # 409009: group already exists on server
+        # 409107: user is already on site
+        # 409011: user is already in group
 
+        group = TSC.GroupItem("test")
+        try:
+            group = server.groups.create(group)
+        except TSC.server.endpoint.exceptions.ServerResponseError as rError:
+            if rError.code == "409009":
+                print("Group already exists")
+                group = server.groups.filter(name=group.name)[0]
+            else:
+                raise rError
+        server.groups.populate_users(group)
+        for user in group.users:
+            print(user.name)
+
+        if args.file:
+            filepath = os.path.abspath(args.file)
+            print("Add users to site from file {}:".format(filepath))
+            added: List[TSC.UserItem]
+            failed: List[TSC.UserItem, TSC.ServerResponseError]
+            added, failed = server.users.create_from_file(filepath)
+            for user, error in failed:
+                print(user, error.code)
+                if error.code == "409017":
+                    user = server.users.filter(name=user.name)[0]
+                    added.append(user)
+            print("Adding users to group:{}".format(added))
+            for user in added:
+                print("Adding user {}".format(user))
+                try:
+                    server.groups.add_user(group, user.id)
+                except ServerResponseError as serverError:
+                    if serverError.code == "409011":
+                        print("user {} is already a member of group {}".format(user.name, group.name))
+                    else:
+                        raise rError
+
+        for user in group.users:
+            print(user.name)
 
 if __name__ == "__main__":
     main()
