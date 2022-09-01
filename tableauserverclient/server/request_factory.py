c1295c6524a191137c62c48a29940639fe1aa12d
jorwoods <jorwoods@users.noreply.github.com>
2022-09-01 00:56:29 -0500

    Use explicit None identity check for datasource updates (#1099)
    Resolves #1062 - cannot set empty password

--- tableauserverclient/server/request_factory.py
+++ tableauserverclient/server/request_factory.py
@@ -16,8 +16,6 @@ from ..models import SubscriptionItem
 from ..models import TaskItem, UserItem, GroupItem, PermissionsRule, FavoriteItem
 from ..models import WebhookItem
 
-from typing import Any, Dict, List, Optional, TYPE_CHECKING, Tuple, Iterable
-
 if TYPE_CHECKING:
     from ..models import SubscriptionItem
     from ..models import DataAlertItem
