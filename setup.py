f653e15b582ae2ea7fc76f423f178d430e2a30ed
Jac <jacalata@users.noreply.github.com>
2022-09-20 02:13:05 -0700

    Development (#1114)
    git cleanup merge: make the development branch really believe it is up to date with master

--- setup.py
+++ setup.py
@@ -14,9 +14,11 @@ setup(
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     # not yet sure how to move this to pyproject.toml
-    packages=['tableauserverclient',
-              'tableauserverclient.helpers',
-              'tableauserverclient.models',
-              'tableauserverclient.server',
-              'tableauserverclient.server.endpoint'],
+    packages=[
+        "tableauserverclient",
+        "tableauserverclient.helpers",
+        "tableauserverclient.models",
+        "tableauserverclient.server",
+        "tableauserverclient.server.endpoint",
+    ],
 )
