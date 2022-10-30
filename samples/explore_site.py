0b4997150874492c5a4d45b7edd818d898fff6ce
Jac Fitzgerald <jac.fitzgerald@salesforce.com>
2022-10-30 00:14:42 -0700

    clean up use of sites
    Remove some warnings that aren't really necessary, remove some print statements, add some logging for the sample.

--- samples/explore_site.py
+++ samples/explore_site.py
@@ -37,7 +37,6 @@ def main():
     parser.add_argument("--new_site_name")
     parser.add_argument("--user_quota")
     parser.add_argument("--storage_quota")
-    parser.add_argument("--status")
 
     args = parser.parse_args()
 
@@ -46,16 +45,20 @@ def main():
     logging.basicConfig(level=logging_level)
 
     # SIGN IN
+    print("Logging into `{}`".format(args.site))
     tableau_auth = TSC.PersonalAccessTokenAuth(args.token_name, args.token_value, site_id=args.site)
     server = TSC.Server(args.server, use_server_version=True)
-    new_site = None
     with server.auth.sign_in(tableau_auth):
         current_site = server.sites.get_by_id(server.site_id)
+        print(current_site)
+        current_site = server.sites.get_by_content_url(args.site)
+        print(current_site)
+        current_site = server.sites.get_by_name(current_site.name)
+        print(current_site)
 
         if args.delete:
-            print("You can only delete the site you are currently in")
-            print("Delete site `{}`?".format(current_site.name))
-            # server.sites.delete(server.site_id)
+            print("Warning: You are deleting the site you are currently in")
+            server.sites.delete(server.site_id)
 
         elif args.create:
             new_site = TSC.SiteItem(args.create, args.url or args.create)
@@ -65,16 +68,23 @@ def main():
             # if a PAT is required, that means going to the UI to create one
 
         else:
+            print("Updating site...")
             new_site = current_site
-            print(current_site, "current user quota:", current_site.user_quota)
-            print("Remember, you can only update the site you are currently in")
-            if args.url:
-                new_site.content_url = args.url
             if args.user_quota:
                 new_site.user_quota = args.user_quota
+            if args.url:
+                new_site.content_url = args.url
+            else:
+                new_site.content_url = current_site.content_url + "_updated"
+            if args.new_site_name:
+                new_site.name = args.new_site_name
+            if args.storage_quota:
+                new_site.storage_quota = args.storage_quota
             try:
                 updated_site = server.sites.update(new_site)
-                print(updated_site, "new user quota:", updated_site.user_quota)
+                print(updated_site)
+                if args.user_quota:
+                    print(updated_site, "new user quota:", updated_site.user_quota)
             except TSC.ServerResponseError as e:
                 print(e)
 
