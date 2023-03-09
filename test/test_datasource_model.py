ec37de223fa4297e9b49f61e104a4c352a39deba
Jac <jacalata@users.noreply.github.com>
2023-03-08 20:52:56 -0800

    add query-tagging attribute to connection (#1202)
    * add query-tagging attribute to connection
    * add explanation for why it doesn't work on hyper

--- test/test_datasource_model.py
+++ test/test_datasource_model.py
@@ -1,5 +1,4 @@
 import unittest
-
 import tableauserverclient as TSC
 
 
@@ -9,3 +8,13 @@ class DatasourceModelTests(unittest.TestCase):
         datasource = TSC.DatasourceItem("10")
         with self.assertRaises(ValueError):
             datasource.project_id = None
+
+    def test_require_boolean_flag_bridge_fail(self):
+        datasource = TSC.DatasourceItem("10")
+        with self.assertRaises(ValueError):
+            datasource.use_remote_query_agent = "yes"
+
+    def test_require_boolean_flag_bridge_ok(self):
+        datasource = TSC.DatasourceItem("10")
+        datasource.use_remote_query_agent = True
+        self.assertEqual(datasource.use_remote_query_agent, True)
