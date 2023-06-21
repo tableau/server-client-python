35491173f68da76b244cdbbdf8164a284d075027
Jordan Woods <lenluin@gmail.com>
2023-06-20 20:17:33 -0500

    fix: remove logging configuration from TSC

--- tableauserverclient/helpers/logging.py
+++ tableauserverclient/helpers/logging.py
@@ -2,5 +2,3 @@ import logging
 
 # TODO change: this defaults to logging *everything* to stdout
 logger = logging.getLogger("TSC")
-logger.setLevel(logging.DEBUG)
-logger.addHandler(logging.StreamHandler())
