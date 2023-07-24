04d9845410bd8172a66c003c1e945160aa4c4750
Lars Breddemann <lbreddemann@salesforce.com>
2023-07-24 17:02:56 +1000

    removed trailing space from comment

--- tableauserverclient/server/filter.py
+++ tableauserverclient/server/filter.py
@@ -11,7 +11,7 @@ class Filter(object):
     def __str__(self):
         value_string = str(self._value)
         if isinstance(self._value, list):
-            # this should turn the string representation of the list 
+            # this should turn the string representation of the list
             # from ['<string1>', '<string2>', ...]
             # to [<string1>,<string2>]
             # so effectively, remove any spaces between "," and "'" and then remove all "'"
