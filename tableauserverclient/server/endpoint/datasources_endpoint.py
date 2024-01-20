68b915774737083ecad5f365f1946a4ef778050f
markm <mark@securehst.com>
2024-01-20 01:44:20 -0600

    Changes to alter cgi dependency to email.Messages

--- tableauserverclient/server/endpoint/datasources_endpoint.py
+++ tableauserverclient/server/endpoint/datasources_endpoint.py
@@ -439,7 +439,7 @@ class Datasources(QuerysetEndpoint):
         with closing(self.get_request(url, parameters={"stream": True})) as server_response:
             m = Message()
             m["Content-Disposition"] = server_response.headers["Content-Disposition"]
-            params = m.get_filename()
+            params = m.get_filename(failobj="")
             if isinstance(filepath, io_types_w):
                 for chunk in server_response.iter_content(1024):  # 1KB
                     filepath.write(chunk)
--- tableauserverclient/server/endpoint/flows_endpoint.py
+++ tableauserverclient/server/endpoint/flows_endpoint.py
@@ -122,7 +122,7 @@ class Flows(QuerysetEndpoint):
         with closing(self.get_request(url, parameters={"stream": True})) as server_response:
             m = Message()
             m["Content-Disposition"] = server_response.headers["Content-Disposition"]
-            params = m.get_filename()
+            params = m.get_filename(failobj="")
             if isinstance(filepath, io_types_w):
                 for chunk in server_response.iter_content(1024):  # 1KB
                     filepath.write(chunk)
