643d7657fa7f5f92959b8742778218ea6f05cdd1
Jordan Woods <13803242+jorwoods@users.noreply.github.com>
2024-07-24 05:33:27 -0500

    chore: switch to pytest style asserts

--- test/test_linked_tasks.py
+++ test/test_linked_tasks.py
@@ -27,47 +27,49 @@ class TestLinkedTasks(unittest.TestCase):
     def test_parse_linked_task_flow_run(self):
         xml = fromstring(GET_LINKED_TASKS.read_bytes())
         task_runs = LinkedTaskFlowRunItem._parse_element(xml, self.server.namespace)
-        self.assertEqual(1, len(task_runs))
+        assert 1 == len(task_runs)
         task = task_runs[0]
-        self.assertEqual(task.flow_run_id, "e3d1fc25-5644-4e32-af35-58dcbd1dbd73")
-        self.assertEqual(task.flow_run_priority, 1)
-        self.assertEqual(task.flow_run_consecutive_failed_count, 3)
-        self.assertEqual(task.flow_run_task_type, "runFlow")
-        self.assertEqual(task.flow_id, "ab1231eb-b8ca-461e-a131-83f3c2b6a673")
-        self.assertEqual(task.flow_name, "flow-name")
+        assert task.flow_run_id == "e3d1fc25-5644-4e32-af35-58dcbd1dbd73"
+        assert task.flow_run_priority == 1
+        assert task.flow_run_consecutive_failed_count == 3
+        assert task.flow_run_task_type == "runFlow"
+        assert task.flow_id == "ab1231eb-b8ca-461e-a131-83f3c2b6a673"
+        assert task.flow_name == "flow-name"
 
     def test_parse_linked_task_step(self):
         xml = fromstring(GET_LINKED_TASKS.read_bytes())
         steps = LinkedTaskStepItem.from_task_xml(xml, self.server.namespace)
-        self.assertEqual(1, len(steps))
+        assert 1 == len(steps)
         step = steps[0]
-        self.assertEqual(step.id, "f554a4df-bb6f-4294-94ee-9a709ef9bda0")
-        self.assertTrue(step.stop_downstream_on_failure)
-        self.assertEqual(step.step_number, 1)
-        self.assertEqual(1, len(step.task_details))
+        assert step.id == "f554a4df-bb6f-4294-94ee-9a709ef9bda0"
+        assert step.stop_downstream_on_failure
+        assert step.step_number == 1
+        assert 1 == len(step.task_details)
         task = step.task_details[0]
-        self.assertEqual(task.flow_run_id, "e3d1fc25-5644-4e32-af35-58dcbd1dbd73")
-        self.assertEqual(task.flow_run_priority, 1)
-        self.assertEqual(task.flow_run_consecutive_failed_count, 3)
-        self.assertEqual(task.flow_run_task_type, "runFlow")
-        self.assertEqual(task.flow_id, "ab1231eb-b8ca-461e-a131-83f3c2b6a673")
-        self.assertEqual(task.flow_name, "flow-name")
+        assert task.flow_run_id == "e3d1fc25-5644-4e32-af35-58dcbd1dbd73"
+        assert task.flow_run_priority == 1
+        assert task.flow_run_consecutive_failed_count == 3
+        assert task.flow_run_task_type == "runFlow"
+        assert task.flow_id == "ab1231eb-b8ca-461e-a131-83f3c2b6a673"
+        assert task.flow_name == "flow-name"
 
     def test_parse_linked_task(self):
         tasks = LinkedTaskItem.from_response(GET_LINKED_TASKS.read_bytes(), self.server.namespace)
-        self.assertEqual(1, len(tasks))
+        assert 1 == len(tasks)
         task = tasks[0]
-        self.assertEqual(task.id, "1b8211dc-51a8-45ce-a831-b5921708e03e")
-        self.assertEqual(task.num_steps, 1)
-        self.assertEqual(task.schedule.id, "be077332-d01d-481b-b2f3-917e463d4dca")
+        assert task.id == "1b8211dc-51a8-45ce-a831-b5921708e03e"
+        assert task.num_steps == 1
+        assert task.schedule is not None
+        assert task.schedule.id == "be077332-d01d-481b-b2f3-917e463d4dca"
 
     def test_get_linked_tasks(self):
         with requests_mock.mock() as m:
             m.get(self.baseurl, text=GET_LINKED_TASKS.read_text())
             tasks, pagination_item = self.server.linked_tasks.get()
 
-        self.assertEqual(1, len(tasks))
+        assert 1 == len(tasks)
         task = tasks[0]
-        self.assertEqual(task.id, "1b8211dc-51a8-45ce-a831-b5921708e03e")
-        self.assertEqual(task.num_steps, 1)
-        self.assertEqual(task.schedule.id, "be077332-d01d-481b-b2f3-917e463d4dca")
+        assert task.id == "1b8211dc-51a8-45ce-a831-b5921708e03e"
+        assert task.num_steps == 1
+        assert task.schedule is not None
+        assert task.schedule.id == "be077332-d01d-481b-b2f3-917e463d4dca"
