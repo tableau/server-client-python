a50ccaa5a0dbe34df337e6db46a968a6a7d7c292
Jordan Woods <13803242+jorwoods@users.noreply.github.com>
2024-07-29 21:31:21 -0500

    docs: publish docstring

--- tableauserverclient/server/endpoint/virtual_connections_endpoint.py
+++ tableauserverclient/server/endpoint/virtual_connections_endpoint.py
@@ -110,6 +110,22 @@ class VirtualConnections(QuerysetEndpoint[VirtualConnectionItem]):
         mode: str = "CreateNew",
         publish_as_draft: bool = False,
     ) -> VirtualConnectionItem:
+        """
+        Publish a virtual connection to the server. 
+
+        For the virtual_connection object, name, project_id, and owner_id are
+        required.
+
+        The virtual_connection_content can be a json string or a file path to a
+        json file. 
+
+        The mode can be "CreateNew" or "Overwrite". If mode is
+        "Overwrite" and the virtual connection already exists, it will be
+        overwritten. 
+
+        If publish_as_draft is True, the virtual connection will be published
+        as a draft, and the id of the draft will be on the response object.
+        """
         try:
             json.loads(virtual_connection_content)
         except json.JSONDecodeError:
