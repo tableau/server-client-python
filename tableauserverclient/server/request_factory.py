215269dc8346eb374158c1abc9349783f9a3b628
jac.fitzgerald <jac.fitzgerald@salesforce.com>
2024-09-19 02:23:25 -0700

    Update request_factory.py

--- tableauserverclient/server/request_factory.py
+++ tableauserverclient/server/request_factory.py
@@ -217,6 +217,8 @@ class DatasourceRequest:
 
         if datasource_item.certification_note:
             datasource_element.attrib["certificationNote"] = str(datasource_item.certification_note)
+        if datasource_item.description is not None:
+            datasource_element.attrib["description"] = str(datasource_item.description)
         if datasource_item.encrypt_extracts is not None:
             datasource_element.attrib["encryptExtracts"] = str(datasource_item.encrypt_extracts).lower()
 
@@ -996,9 +998,9 @@ class WorkbookRequest:
             if data_freshness_policy_config.option == "FreshEvery":
                 if data_freshness_policy_config.fresh_every_schedule is not None:
                     fresh_every_element = ET.SubElement(data_freshness_policy_element, "freshEverySchedule")
-                    fresh_every_element.attrib["frequency"] = (
-                        data_freshness_policy_config.fresh_every_schedule.frequency
-                    )
+                    fresh_every_element.attrib[
+                        "frequency"
+                    ] = data_freshness_policy_config.fresh_every_schedule.frequency
                     fresh_every_element.attrib["value"] = str(data_freshness_policy_config.fresh_every_schedule.value)
                 else:
                     raise ValueError(f"data_freshness_policy_config.fresh_every_schedule must be populated.")
