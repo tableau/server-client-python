acd2c4922b52c41055ca599b90f3a2449127afec
jac.fitzgerald <jac.fitzgerald@salesforce.com>
2024-10-04 18:29:33 -0700

    update versioneer, defusedxml

--- setup.py
+++ setup.py
@@ -1,15 +1,6 @@
 import versioneer
 from setuptools import setup
 
-"""
-once versioneer 0.25 gets released, we can move this from setup.cfg to pyproject.toml
-[tool.versioneer]
-VCS = "git"
-style = "pep440-pre"
-versionfile_source = "tableauserverclient/_version.py"
-versionfile_build = "tableauserverclient/_version.py"
-tag_prefix = "v"
-"""
 setup(
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
--- tableauserverclient/__init__.py
+++ tableauserverclient/__init__.py
@@ -133,3 +133,6 @@ __all__ = [
     "WeeklyInterval",
     "WorkbookItem",
 ]
+
+from . import _version
+__version__ = _version.get_versions()['version']
