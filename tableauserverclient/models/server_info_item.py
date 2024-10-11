f8728b211d8e8eb4507f6e907e482da5a60d3577
Jordan Woods <jorwoods@users.noreply.github.com>
2024-10-11 15:58:32 -0500

    docs: docstrings for Server and ServerInfo (#1494)
    Co-authored-by: Jordan Woods <13803242+jorwoods@users.noreply.github.com>

--- tableauserverclient/models/server_info_item.py
+++ tableauserverclient/models/server_info_item.py
@@ -7,6 +7,28 @@ from tableauserverclient.helpers.logging import logger
 
 
 class ServerInfoItem:
+    """
+    The ServerInfoItem class contains the build and version information for
+    Tableau Server. The server information is accessed with the
+    server_info.get() method, which returns an instance of the ServerInfo class.
+
+    Attributes
+    ----------
+    product_version : str
+        Shows the version of the Tableau Server or Tableau Cloud
+        (for example, 10.2.0).
+
+    build_number : str
+        Shows the specific build number (for example, 10200.17.0329.1446).
+
+    rest_api_version : str
+        Shows the supported REST API version number. Note that this might be
+        different from the default value specified for the server, with the
+        Server.version attribute. To take advantage of new features, you should
+        query the server and set the Server.version to match the supported REST
+        API version number.
+    """
+
     def __init__(self, product_version, build_number, rest_api_version):
         self._product_version = product_version
         self._build_number = build_number
