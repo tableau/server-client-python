a661d8ed6f8e1dabcaa1d0f050cb40d67695c9cd
john <he.j@salesforce.com>
2025-05-05 09:19:12 -0700

    remove the redudant function

--- tableauserverclient/server/endpoint/metadata_endpoint.py
+++ tableauserverclient/server/endpoint/metadata_endpoint.py
@@ -68,7 +68,7 @@ class Metadata(Endpoint):
             raise InvalidGraphQLQuery("Must provide a string")
 
         # Setting content type because post_reuqest defaults to text/xml
-        server_response = self.post_request(url, graphql_query, content_type="application/json", parameters=parameters)
+        server_response = self.post_request(url, graphql_query, parameters=parameters)
         results = server_response.json()
 
         if abort_on_error and results.get("errors", None):
