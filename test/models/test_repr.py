a588d18c7ab3b04140346f29fdeee77af67aecbc
Jac Fitzgerald <jacalata@gmail.com>
2025-05-15 22:02:29 -0700

    add init files to find test_repr, fix it to pass
    black

--- test/models/test_repr.py
+++ test/models/test_repr.py
@@ -1,14 +1,13 @@
 import inspect
 
 from unittest import TestCase
-import _models  # type: ignore  # did not set types for this
+import tableauserverclient.models as TSC_models  # type: ignore  # did not set types for this
 import tableauserverclient as TSC
 
 from typing import Any
 
 
-# ensure that all models that don't need parameters can be instantiated
-# todo....
+# ensure that all models can be instantiated
 def instantiate_class(name: str, obj: Any):
     # Get the constructor (init) of the class
     constructor = getattr(obj, "__init__", None)
@@ -31,14 +30,38 @@ def instantiate_class(name: str, obj: Any):
         print(f"Class '{name}' does not have a constructor (__init__ method).")
 
 
+not_yet_done = [
+    "DQWItem",
+    "UnpopulatedPropertyError",
+    "FavoriteItem",
+    "FileuploadItem",
+    "FlowRunItem",
+    "IntervalItem",
+    "LinkedTaskItem",
+    "LinkedTaskStepItem",
+    "LinkedTaskFlowRunItem",
+    "Permission",
+    "SiteAuthConfiguration",
+    "Resource",
+    "TagItem",
+    "ExtractItem",
+]
+
+
 class TestAllModels(TestCase):
-    # not all models have __repr__ yet: see above list
+
+    # confirm that all models can be instantiated without params, and have __repr__ implemented
+    # not all do have __repr__ yet: see above list 'not_yet_done'
     def test_repr_is_implemented(self):
-        m = _models.get_defined_models()
-        for model in m:
-            with self.subTest(model.__name__, model=model):
-                print(model.__name__, type(model.__repr__).__name__)
-                self.assertEqual(type(model.__repr__).__name__, "function")
+        m = TSC_models
+        for type_name in m.__dict__:
+            if type_name in not_yet_done:
+                continue
+            model = getattr(m, type_name)
+            if inspect.isclass(model):
+                with self.subTest(type_name):
+                    self.assertTrue(hasattr(model, "__repr__"))
+                    self.assertEqual(type(model.__repr__).__name__, "function")
 
     # 2 - Iterate through the objects in the module
     def test_by_reflection(self):
