bd05f1c597ee79d4e6ad50e3903eeec783126980
SFDC\vchavatapalli <chvineeth.2@gmail.com>
2025-07-23 10:33:17 -0700

    Added assertions for test cases

--- test/test_datasource.py
+++ test/test_datasource.py
@@ -232,6 +232,7 @@ def test_update_copy_fields(server) -> None:
             updated_ids = [conn.id for conn in connection_items]
 
             self.assertEqual(updated_ids, connection_luids)
+            self.assertEqual("auth-keypair",connection_items[0].auth_type)
 
     def test_populate_permissions(self) -> None:
         with open(asset(POPULATE_PERMISSIONS_XML), "rb") as f:
