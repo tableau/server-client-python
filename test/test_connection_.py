23a71f52bdae856b67e4f158d3c56800c7453e18
Jordan Woods <13803242+jorwoods@users.noreply.github.com>
2025-08-29 08:07:13 -0500

    chore: pytestify test_connection_
    Embrace pytest's testing methodology in test_connection_

--- test/test_connection_.py
+++ test/test_connection_.py
@@ -1,34 +1,38 @@
-import unittest
 import tableauserverclient as TSC
 
+import pytest
 
-class DatasourceModelTests(unittest.TestCase):
-    def test_require_boolean_query_tag_fails(self):
-        conn = TSC.ConnectionItem()
-        conn._connection_type = "postgres"
-        with self.assertRaises(ValueError):
-            conn.query_tagging = "no"
 
-    def test_set_query_tag_normal_conn(self):
-        conn = TSC.ConnectionItem()
-        conn._connection_type = "postgres"
-        conn.query_tagging = True
-        self.assertEqual(conn.query_tagging, True)
+def test_require_boolean_query_tag_fails():
+    conn = TSC.ConnectionItem()
+    conn._connection_type = "postgres"
+    with pytest.raises(ValueError):
+        conn.query_tagging = "no"
 
-    def test_ignore_query_tag_for_hyper(self):
-        conn = TSC.ConnectionItem()
-        conn._connection_type = "hyper"
-        conn.query_tagging = True
-        self.assertEqual(conn.query_tagging, None)
 
-    def test_ignore_query_tag_for_teradata(self):
-        conn = TSC.ConnectionItem()
-        conn._connection_type = "teradata"
-        conn.query_tagging = True
-        self.assertEqual(conn.query_tagging, None)
+def test_set_query_tag_normal_conn():
+    conn = TSC.ConnectionItem()
+    conn._connection_type = "postgres"
+    conn.query_tagging = True
+    assert conn.query_tagging
 
-    def test_ignore_query_tag_for_snowflake(self):
-        conn = TSC.ConnectionItem()
-        conn._connection_type = "snowflake"
-        conn.query_tagging = True
-        self.assertEqual(conn.query_tagging, None)
+
+def test_ignore_query_tag_for_hyper():
+    conn = TSC.ConnectionItem()
+    conn._connection_type = "hyper"
+    conn.query_tagging = True
+    assert conn.query_tagging is None
+
+
+def test_ignore_query_tag_for_teradata():
+    conn = TSC.ConnectionItem()
+    conn._connection_type = "teradata"
+    conn.query_tagging = True
+    assert conn.query_tagging is None
+
+
+def test_ignore_query_tag_for_snowflake():
+    conn = TSC.ConnectionItem()
+    conn._connection_type = "snowflake"
+    conn.query_tagging = True
+    assert conn.query_tagging is None
