0b4403204e3f05031957f8e55947574f8d690a98
Jordan Woods <13803242+jorwoods@users.noreply.github.com>
2025-08-30 08:59:43 -0500

    chore: add type hints to tests

--- test/test_connection_.py
+++ test/test_connection_.py
@@ -3,14 +3,14 @@ import tableauserverclient as TSC
 import pytest
 
 
-def test_require_boolean_query_tag_fails():
+def test_require_boolean_query_tag_fails() -> None:
     conn = TSC.ConnectionItem()
     conn._connection_type = "postgres"
     with pytest.raises(ValueError):
-        conn.query_tagging = "no"
+        conn.query_tagging = "no"  # type: ignore[assignment]
 
 
-def test_set_query_tag_normal_conn():
+def test_set_query_tag_normal_conn() -> None:
     conn = TSC.ConnectionItem()
     conn._connection_type = "postgres"
     conn.query_tagging = True
@@ -18,7 +18,7 @@ def test_set_query_tag_normal_conn():
 
 
 @pytest.mark.parametrize("conn_type", ["hyper", "teradata", "snowflake"])
-def test_ignore_query_tag(conn_type):
+def test_ignore_query_tag(conn_type: str) -> None:
     conn = TSC.ConnectionItem()
     conn._connection_type = conn_type
     conn.query_tagging = True
