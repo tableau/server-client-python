bb894db60c008536a6dedc702ecb6dd38d96ef46
Jordan Woods <jorwoods@users.noreply.github.com>
2025-12-23 14:23:09 -0600

    chore: pytestify sort (#1701)
    * fix: black ci errors
    
    * chore: pytestify sort
    
    ---------
    
    Co-authored-by: Jordan Woods <13803242+jorwoods@users.noreply.github.com>

--- test/test_sort.py
+++ test/test_sort.py
@@ -1,3 +1,5 @@
+import re
+import unittest
 from urllib.parse import parse_qs
 
 import pytest
@@ -11,33 +13,30 @@ def server():
     """Fixture to create a TSC.Server instance for testing."""
     server = TSC.Server("http://test", False)
 
-    # Fake signin
-    server._site_id = "dad65087-b08b-4603-af4e-2887b8aafc67"
-    server._auth_token = "j80k54ll2lfMZ0tv97mlPvvSCRyD0DOM"
+    def test_empty_filter(self):
+        with pytest.raises(TypeError):
+            TSC.Filter("")
 
     return server
 
+            resp = self.server.workbooks.get_request(url, request_object=opts)
+            query = parse_qs(resp.request.query)
+            assert "pagenumber" in query
+            assert query["pagenumber"] == ["13"]
+            assert "pagesize" in query
+            assert query["pagesize"] == ["13"]
+            assert "filter" in query
+            assert query["filter"] == ["name:eq:superstore"]
 
-def test_empty_filter() -> None:
-    with pytest.raises(TypeError):
-        TSC.Filter("")  # type: ignore
+    def test_filter_equals_list(self):
+        with pytest.raises(ValueError, match="Filter values can only be a list if the operator is 'in'.") as cm:
+            TSC.Filter(TSC.RequestOptions.Field.Tags, TSC.RequestOptions.Operator.Equals, ["foo", "bar"])
 
-
-def test_filter_equals(server: TSC.Server) -> None:
-    with requests_mock.mock() as m:
-        m.get(requests_mock.ANY)
-        url = "http://test/api/2.3/sites/dad65087-b08b-4603-af4e-2887b8aafc67/workbooks"
-        opts = TSC.RequestOptions(pagesize=13, pagenumber=13)
-        opts.filter.add(TSC.Filter(TSC.RequestOptions.Field.Name, TSC.RequestOptions.Operator.Equals, "Superstore"))
-
-        resp = server.workbooks.get_request(url, request_object=opts)
-        query = parse_qs(resp.request.query)
-        assert "pagenumber" in query
-        assert query["pagenumber"] == ["13"]
-        assert "pagesize" in query
-        assert query["pagesize"] == ["13"]
-        assert "filter" in query
-        assert query["filter"] == ["name:eq:superstore"]
+    def test_filter_in(self):
+        with requests_mock.mock() as m:
+            m.get(requests_mock.ANY)
+            url = "http://test/api/2.3/sites/dad65087-b08b-4603-af4e-2887b8aafc67/workbooks"
+            opts = TSC.RequestOptions(pagesize=13, pagenumber=13)
 
 
 def test_filter_equals_list() -> None:
@@ -94,14 +93,50 @@ def test_filter_combo(server: TSC.Server) -> None:
             )
         )
 
-        opts.filter.add(TSC.Filter(TSC.RequestOptions.Field.SiteRole, TSC.RequestOptions.Operator.Equals, "Publisher"))
+            resp = self.server.workbooks.get_request(url, request_object=opts)
+            query = parse_qs(resp.request.query)
+            assert "pagenumber" in query
+            assert query["pagenumber"] == ["13"]
+            assert "pagesize" in query
+            assert query["pagesize"] == ["13"]
+            assert "filter" in query
+            assert query["filter"] == ["tags:in:[stocks,market]"]
 
         resp = server.workbooks.get_request(url, request_object=opts)
 
-        query = parse_qs(resp.request.query)
-        assert "pagenumber" in query
-        assert query["pagenumber"] == ["13"]
-        assert "pagesize" in query
-        assert query["pagesize"] == ["13"]
-        assert "filter" in query
-        assert query["filter"] == ["lastlogin:gte:2017-01-15t00:00:00:00z,siterole:eq:publisher"]
+            resp = self.server.workbooks.get_request(url, request_object=opts)
+            query = parse_qs(resp.request.query)
+            assert "pagenumber" in query
+            assert query["pagenumber"] == ["13"]
+            assert "pagesize" in query
+            assert query["pagesize"] == ["13"]
+            assert "sort" in query
+            assert query["sort"] == ["name:asc"]
+
+    def test_filter_combo(self):
+        with requests_mock.mock() as m:
+            m.get(requests_mock.ANY)
+            url = "http://test/api/2.3/sites/dad65087-b08b-4603-af4e-2887b8aafc67/users"
+            opts = TSC.RequestOptions(pagesize=13, pagenumber=13)
+
+            opts.filter.add(
+                TSC.Filter(
+                    TSC.RequestOptions.Field.LastLogin,
+                    TSC.RequestOptions.Operator.GreaterThanOrEqual,
+                    "2017-01-15T00:00:00:00Z",
+                )
+            )
+
+            opts.filter.add(
+                TSC.Filter(TSC.RequestOptions.Field.SiteRole, TSC.RequestOptions.Operator.Equals, "Publisher")
+            )
+
+            resp = self.server.workbooks.get_request(url, request_object=opts)
+
+            query = parse_qs(resp.request.query)
+            assert "pagenumber" in query
+            assert query["pagenumber"] == ["13"]
+            assert "pagesize" in query
+            assert query["pagesize"] == ["13"]
+            assert "filter" in query
+            assert query["filter"] == ["lastlogin:gte:2017-01-15t00:00:00:00z,siterole:eq:publisher"]
