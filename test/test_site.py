f991c67331f5fd5c70424241ef68568a8fc0a38c
Jordan Woods <jorwoods@users.noreply.github.com>
2025-12-23 14:26:10 -0600

    chore: pytestify test_site (#1699)
    * fix: black ci errors
    
    * chore: pytestify test_site
    
    * chore: pytestify test_site_model
    
    ---------
    
    Co-authored-by: Jordan Woods <13803242+jorwoods@users.noreply.github.com>

--- test/test_site.py
+++ test/test_site.py
@@ -1,6 +1,5 @@
 from itertools import product
-import os.path
-import unittest
+from pathlib import Path
 
 from defusedxml import ElementTree as ET
 import pytest
@@ -8,8 +7,6 @@ import requests_mock
 
 import tableauserverclient as TSC
 from tableauserverclient.server.request_factory import RequestFactory
-
-from . import _utils
 
 from . import _utils
 
@@ -273,16 +270,40 @@ def test_encrypt(server: TSC.Server) -> None:
         server.sites.encrypt_extracts("0626857c-1def-4503-a7d8-7907c3ff9d9f")
 
 
-        assert configs[0].auth_setting == "OIDC"
-        assert configs[0].enabled
-        assert configs[0].idp_configuration_id == "00000000-0000-0000-0000-000000000000"
-        assert configs[0].idp_configuration_name == "Initial Salesforce"
-        assert configs[0].known_provider_alias == "Salesforce"
-        assert configs[1].auth_setting == "SAML"
-        assert configs[1].enabled
-        assert configs[1].idp_configuration_id == "11111111-1111-1111-1111-111111111111"
-        assert configs[1].idp_configuration_name == "Initial SAML"
-        assert configs[1].known_provider_alias is None
+def test_recrypt(server: TSC.Server) -> None:
+    with requests_mock.mock() as m:
+        m.post(server.sites.baseurl + "/0626857c-1def-4503-a7d8-7907c3ff9d9f/reencrypt-extracts", status_code=200)
+        server.sites.re_encrypt_extracts("0626857c-1def-4503-a7d8-7907c3ff9d9f")
+
+
+def test_decrypt(server: TSC.Server) -> None:
+    with requests_mock.mock() as m:
+        m.post(server.sites.baseurl + "/0626857c-1def-4503-a7d8-7907c3ff9d9f/decrypt-extracts", status_code=200)
+        server.sites.decrypt_extracts("0626857c-1def-4503-a7d8-7907c3ff9d9f")
+
+
+def test_list_auth_configurations(server: TSC.Server) -> None:
+    server.version = "3.24"
+    response_xml = SITE_AUTH_CONFIG_XML.read_text()
+
+    assert server.sites.baseurl == server.sites.baseurl
+
+    with requests_mock.mock() as m:
+        m.get(f"{server.sites.baseurl}/{server.site_id}/site-auth-configurations", status_code=200, text=response_xml)
+        configs = server.sites.list_auth_configurations()
+
+    assert len(configs) == 2, "Expected 2 auth configurations"
+
+    assert configs[0].auth_setting == "OIDC"
+    assert configs[0].enabled
+    assert configs[0].idp_configuration_id == "00000000-0000-0000-0000-000000000000"
+    assert configs[0].idp_configuration_name == "Initial Salesforce"
+    assert configs[0].known_provider_alias == "Salesforce"
+    assert configs[1].auth_setting == "SAML"
+    assert configs[1].enabled
+    assert configs[1].idp_configuration_id == "11111111-1111-1111-1111-111111111111"
+    assert configs[1].idp_configuration_name == "Initial SAML"
+    assert configs[1].known_provider_alias is None
 
 
 @pytest.mark.parametrize("capture", [True, False, None])
