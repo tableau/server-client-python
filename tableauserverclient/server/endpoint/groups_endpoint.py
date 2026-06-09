b42e121cbf16d6e4b38e643ac85690cc5a182add
Jordan Woods <13803242+jorwoods@users.noreply.github.com>
2026-06-09 07:11:37 -0500

    style: black

--- tableauserverclient/server/endpoint/groups_endpoint.py
+++ tableauserverclient/server/endpoint/groups_endpoint.py
@@ -181,32 +181,32 @@ class Groups(QuerysetEndpoint[GroupItem]):
     @api(version="2.0")
     def update(self, group_item, as_job=False):
         """
-         Updates a group on the site.
+        Updates a group on the site.
 
-         REST API: https://help.tableau.com/current/api/rest_api/en-us/REST/rest_api_ref_users_and_groups.htm#update_group
+        REST API: https://help.tableau.com/current/api/rest_api/en-us/REST/rest_api_ref_users_and_groups.htm#update_group
 
-         Parameters
-         ----------
-         group_item : GroupItem
-             The group item to update.
+        Parameters
+        ----------
+        group_item : GroupItem
+            The group item to update.
 
-         as_job : bool
-             (Optional) If this value is set to True, the update operation will
-             be asynchronous and return a JobItem. This is only supported for
-             Active Directory groups. By default, this value is set to False.
+        as_job : bool
+            (Optional) If this value is set to True, the update operation will
+            be asynchronous and return a JobItem. This is only supported for
+            Active Directory groups. By default, this value is set to False.
 
-         Returns
-         -------
-         GroupItem | JobItem
+        Returns
+        -------
+        GroupItem | JobItem
 
-         Raises
-         ------
-         MissingRequiredFieldError
-             If the group_item does not have an ID, the method raises an error.
+        Raises
+        ------
+        MissingRequiredFieldError
+            If the group_item does not have an ID, the method raises an error.
 
-         ValueError
-             If the group_item is a local group and as_job is set to True, the
-             method raises an error.
+        ValueError
+            If the group_item is a local group and as_job is set to True, the
+            method raises an error.
         """
         url = f"{self.baseurl}/{group_item.id}"
 
